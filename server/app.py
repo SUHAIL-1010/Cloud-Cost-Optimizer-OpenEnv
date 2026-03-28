b471182896617f16d5438ffc201a4a9b5e359477
SUHAIL-1010 <mohamedsuhail102005@gmail.com>
2026-03-28 17:49:28 +0530

    Add root endpoint to redirect to documentation

--- server/app.py
+++ server/app.py
@@ -1,11 +1,15 @@
 # server/app.py
+from fastapi.responses import RedirectResponse
 from openenv.core.env_server import create_fastapi_app
 from models import CloudCostAction, CloudCostObservation
 from server.environment import CloudCostEnvironment
 
-# We pass the class ITSELF (CloudCostEnvironment), no parentheses!
 app = create_fastapi_app(
     env=CloudCostEnvironment, 
     action_cls=CloudCostAction, 
     observation_cls=CloudCostObservation
 )
+
+@app.get("/")
+def read_root():
+    return RedirectResponse(url="/docs")
